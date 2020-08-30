81ffb200431bd7552e7fe45744306c1afe8cf01f
orkinyo <orkinyo@users.noreply.github.com>
2020-08-30 14:37:53 +0000

    automated commit by check50 [check50=True]

---
+++ recover.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+#include <stdint.h>
+#define block 512
+#define name 8
+typedef uint8_t BYTE;
+int magic[] = {0xff, 0xd8, 0xff,14};
+void inc(int* c1, int* c2, int* c3);
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        printf("Usage ./recover file\n");
+        return 1;
+    }
+    FILE* raw = fopen(argv[1],"r");
+
+    if(raw == NULL)
+    {
+        printf("Cannot open\n");
+        return 1;
+    }
+
+    BYTE buffer[block];
+    bool flagfile = false;
+
+    FILE* img = NULL;
+    int counter = 0;
+
+
+    printf("opened\n");
+
+
+    while(fread(buffer,block,1,raw) == 1)
+    {
+        //printf("h\n");
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            //printf("here\n");
+
+            if(flagfile)
+            {
+                fclose(img);
+            }
+            else
+            {
+                flagfile = true;
+            }
+            char fname[name];
+            sprintf(fname,"%03d.jpg",counter);
+            img = fopen(fname,"w");
+            counter += 1;
+            if(img == NULL)
+            {
+                printf("opening out\n");
+                return 1;
+            }
+
+
+        }
+
+        if(flagfile)
+        {
+            fwrite(buffer,block,1,img);
+        }
+
+    }
+
+    fclose(img);
+    fclose(raw);
+
+
+
+}
+
+
+
