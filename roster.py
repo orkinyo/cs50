32a8d77688070f895700143dec42639efdd5e652
orkinyo <orkinyo@users.noreply.github.com>
2020-09-03 16:57:30 +0000

    automated commit by submit50

--- roster.py
+++ roster.py
@@ -1,1 +1,19 @@
 # TODO
+import cs50
+import sys
+
+def ff(query):
+    for row in query:
+        if row['middle'] == None:
+            print(f"{row['first']} {row['last']}, born {row['birth']}")
+        else:
+            print(f"{row['first']} {row['middle']} {row['last']}, born {row['birth']}")
+
+if len(sys.argv)!=2:
+    print("error")
+    exit(1)
+
+house = sys.argv[1]
+db = cs50.SQL("sqlite:///students.db")
+query = db.execute("SELECT first, middle, last, birth from students where house = ? ORDER BY last,first;",house)
+ff(query)
